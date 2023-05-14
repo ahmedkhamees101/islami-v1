dbd9224249cd462ca5a226259b9272a5387afa30
ahmedkhamees101 <109658883+ahmedkhamees101@users.noreply.github.com>
2023-05-14 15:05:02 +0300

    Update my_theme.dart
    done theme

--- lib/ui/my_theme.dart
+++ lib/ui/my_theme.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
 
 class MyTheme {
-  static const Color lightColor = Color(0xFFB7935F);
+  static const  Color lightColor = Color(0xFFB7935F);
   static ThemeData lightTheme = ThemeData(
     primaryColor: lightColor,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: lightColor,
-      type:BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.black,
-      selectedLabelStyle: TextStyle(fontWeight: FontWeight.w700),
-      unselectedItemColor: Colors.white,
-    ),
+    bottomNavigationBarTheme:bottomNavigationBarTheme(),
     scaffoldBackgroundColor: Colors.transparent,
-    textTheme: const TextTheme(
-      bodyMedium:TextStyle(color: Color(0xFFB7935F),fontSize: 22,
-          fontWeight:FontWeight.w700),
-      bodySmall: TextStyle(color: Color(0xFF242424),fontSize: 28,
-          fontWeight:FontWeight.bold),
-    ),
-
-    appBarTheme:const AppBarTheme(
-      iconTheme:IconThemeData(color: Colors.black,size: 28),
-      backgroundColor: Colors.transparent,
-      elevation: 0.0,
-      centerTitle: true,
-    )
+    textTheme: textTheme(color:Color(0xFFB7935F)),
+    appBarTheme:appBarTheme(),
   );
 
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: lightColor,
+    bottomNavigationBarTheme:bottomNavigationBarTheme(),
+    scaffoldBackgroundColor: Colors.transparent,
+    textTheme: textTheme(color:Color(0xFFB7935F)),
+    appBarTheme:appBarTheme(),
+  );
+}
+ TextTheme  textTheme({required Color color}) {
+  return TextTheme(
+    bodyMedium:TextStyle(color: color,fontSize: 22,
+        fontWeight:FontWeight.w700),
+    bodySmall: TextStyle(color: Color(0xFF943939),fontSize: 28,
+        fontWeight:FontWeight.bold),
+  );
+}
+AppBarTheme appBarTheme(){
+  return AppBarTheme(
+    iconTheme:IconThemeData(color: Colors.black,size: 28),
+    backgroundColor: Colors.transparent,
+    elevation: 0.0,
+    centerTitle: true,
+  );
+}
+BottomNavigationBarThemeData bottomNavigationBarTheme (){
+  return BottomNavigationBarThemeData(
+    backgroundColor: MyTheme.lightColor,
+    type:BottomNavigationBarType.fixed,
+    selectedItemColor: Colors.black,
+    selectedLabelStyle: TextStyle(fontWeight: FontWeight.w700),
+    unselectedItemColor: Colors.white,
+    
+  );
 }
